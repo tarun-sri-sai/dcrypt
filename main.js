1859e3a3436733d8553cd7a1c7f9029c741fa235
tarun-sri-sai <76744890+Tarun-Sri-Sai@users.noreply.github.com>
2024-08-21 22:02:15 +0530

    Add electron, nodemon and tailwind config

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = { production: false, development: true }[process.env.NODE_ENV];
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:3000");
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "build", "index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
