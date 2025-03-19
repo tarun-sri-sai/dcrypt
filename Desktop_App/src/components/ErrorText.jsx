f6779af0938c4e385df8e834620f2ee68213eda0
tarun-sri-sai <76744890+Tarun-Sri-Sai@users.noreply.github.com>
2025-03-19 07:32:50 +0530

    Move extension code to root

--- Desktop_App/src/components/ErrorText.jsx
+++
@@ -1,5 +0,0 @@
-const ErrorText = ({ message }) => {
-  return <>{message && <div className="text-red-500">{message}</div>}</>;
-};
-
-export default ErrorText;
--- Desktop_App/src/constants.jsx
+++
@@ -1,3 +0,0 @@
-export const DIRECTORY_KEY = "dcryptDirectory";
-export const MIN_ITEM_NAME_LENGTH = 32;
-export const INFO_TIMEOUT = 3000;
