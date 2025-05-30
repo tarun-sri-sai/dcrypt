66b0a69400c9a31dc85e61b7cd1498f5487df742
tarun-sri-sai <76744890+Tarun-Sri-Sai@users.noreply.github.com>
2025-05-30 07:09:24 +0530

    Add OpenPGP support

--- src/dcryptEditor.ts
+++ src/dcryptEditor.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
-import * as util from "./util";
-import path from "path";
-import os from "os";
+import * as util from "./util.js";
+import * as openpgp from "openpgp";
 
 export class DcryptEditorProvider
   implements vscode.CustomEditorProvider<vscode.CustomDocument>
@@ -11,12 +10,12 @@ export class DcryptEditorProvider
   > = new vscode.EventEmitter<
     vscode.CustomDocumentEditEvent<vscode.CustomDocument>
   >().event;
-  private readonly passwordStore: Map<string, string>;
+  private readonly passwordStore: Map<string, string[]>;
   private readonly context: vscode.ExtensionContext;
 
   constructor(
     context: vscode.ExtensionContext,
-    passwordStore: Map<string, string>,
+    passwordStore: Map<string, string[]>,
   ) {
     this.context = context;
     this.passwordStore = passwordStore;
@@ -36,35 +35,34 @@ export class DcryptEditorProvider
     _token: vscode.CancellationToken,
   ): Promise<void> {
     const uri = document.uri;
-    const fileContent = await this.readFile(uri);
+    const fileContent = await vscode.workspace.fs.readFile(uri);
 
-    const userDirectory = os.homedir(); // Use the user directory as the key for mapping password
-    let password = this.passwordStore.get(userDirectory);
-    if (!password) {
-      password = await this.promptForPassword(path.basename(userDirectory));
-      if (!password) {
+    const [passwordStoreKey, displayName] = util.getPasswordKey(uri);
+    let passwords = this.passwordStore.get(passwordStoreKey) ?? [""];
+    if (!passwords[0]) {
+      passwords = [(await this.promptForPassword(displayName)) ?? ""];
+      if (!passwords[0]) {
         vscode.window.showErrorMessage("No password was provided");
         setImmediate(() => webviewPanel.dispose());
         return;
       }
-      this.passwordStore.set(userDirectory, password);
+      this.passwordStore.set(passwordStoreKey, passwords);
     }
 
     let decryptedContent = "";
 
     if (fileContent.length > 0) {
       try {
-        const [ivBase64, encryptedContentBase64] = fileContent.split(":");
-        if (ivBase64 && encryptedContentBase64) {
-          decryptedContent = util.decrypt(
-            encryptedContentBase64,
-            ivBase64,
-            password,
-          );
-        }
+        decryptedContent = (
+          await openpgp.decrypt({
+            message: await openpgp.readMessage({ binaryMessage: fileContent }),
+            passwords,
+            format: "utf8",
+          })
+        ).data;
       } catch (error: any) {
         vscode.window.showErrorMessage("Failed to decrypt file");
-        this.passwordStore.delete(userDirectory);
+        this.passwordStore.delete(passwordStoreKey);
         setImmediate(() => webviewPanel.dispose());
         return;
       }
@@ -86,7 +84,7 @@ export class DcryptEditorProvider
             });
             break;
           case "save":
-            await this.saveFile(uri, message.text, password);
+            await this.saveFile(uri, message.text, passwords);
             break;
         }
       },
@@ -108,32 +106,19 @@ export class DcryptEditorProvider
     });
   }
 
-  private async readFile(uri: vscode.Uri): Promise<string> {
-    try {
-      const fileData = await vscode.workspace.fs.readFile(uri);
-      return Buffer.from(fileData).toString("utf-8");
-    } catch (error) {
-      return "";
-    }
-  }
-
   private async saveFile(
     uri: vscode.Uri,
-    content: string,
-    password: string,
+    text: string,
+    passwords: string[],
   ): Promise<void> {
     try {
-      const iv = util.getRandomIv();
-      const ivBase64 = iv.toString("base64");
+      const fileContent = await openpgp.encrypt({
+        message: await openpgp.createMessage({ text }),
+        passwords,
+        format: "binary",
+      });
 
-      const encryptedContent = util.encrypt(content, iv, password);
-
-      const fileContent = `${ivBase64}:${encryptedContent}`;
-
-      await vscode.workspace.fs.writeFile(
-        uri,
-        Buffer.from(fileContent, "utf-8"),
-      );
+      await vscode.workspace.fs.writeFile(uri, fileContent);
     } catch (error: any) {
       vscode.window.showErrorMessage(
         `Failed to save encrypted file: ${error.message}`,
